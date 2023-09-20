2aec8cbb3665570baa04d5f50cb789c7338e8f73
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-09-21 02:58:50 +0530

    Update Note.jsx
    added useDrag and useDrop for drag and drop and also used index for the unique item.

--- frontend-react/src/components/Note.jsx
+++ frontend-react/src/components/Note.jsx
@@ -1,39 +1,90 @@
 import React from "react";
+import { useDrag, useDrop } from 'react-dnd';
 
-function Note(props) {
+const Note = ({ id, title, onDelete, dark, isChecked, handleCheckboxChange, index, moveNote }) => {
+  const [, ref, preview] = useDrag({
+      type: 'NOTE',
+      item: { index }
+  });
 
-  const { id, title, dark, isChecked, handleCheckboxChange, onDelete } = props;
+  const [, drop] = useDrop({
+      accept: 'NOTE',
+      hover: (draggedItem) => {
+          if (draggedItem.index !== index) {
+              moveNote(draggedItem.index, index);
+              draggedItem.index = index;
+          }
+      }
+  });
 
-  // console.log()
-  
+  const handleDrag = (e) => {
+      e.preventDefault();
+  }
+
   function handleClick() {
     console.log("passed the id " + id);
     onDelete(id);
   }
-  
-  console.log(`Note id: ${id}, isChecked: ${isChecked[id]}`);
-  
+
   return (
-    <div className={`div-main ${dark ? "div-main-dark" : ""}`}>
-      &nbsp;
-
       <div 
-        type="text" 
-        className={`checkbox--label ${isChecked[id] ? "checked--Input" : ""}`}
-        name="checkbox--tasks" 
-        checked={isChecked[id]} 
-        onClick={() => handleCheckboxChange(id)}
-      /> 
-      
-      <div className={`div--tasks ${isChecked[id] ? "completed--task" : ""}`}>
-          {title}
-      </div>
+          ref={(node) => ref(drop(node))}
+      >
+          <div className={`div-main ${dark ? "div-main-dark" : ""} ${index === 0 ? 'unique' : ''}`}>
+            &nbsp;
 
-      <button className="button__delete" onClick={handleClick}>
-        <img className="cross__icon" src="../../assets/images/icon-cross.svg" alt="cross icon" />
-      </button>
-    </div>
+            <div 
+              type="text" 
+              className={`checkbox--label ${isChecked[id] ? "checked--Input" : ""}`}
+              name="checkbox--tasks" 
+              checked={isChecked[id]} 
+              onClick={() => handleCheckboxChange(id)}
+            /> 
+            
+            <div className={`div--tasks ${isChecked[id] ? "completed--task" : ""}`}>
+                {title}
+            </div>
+
+            <button className="button__delete" onClick={handleClick}>
+              <img className="cross__icon" src="../../assets/images/icon-cross.svg" alt="cross icon" />
+            </button>
+          </div>
+      </div>
   );
 }
+
+// function Note(props) {
+
+//   const { id, title, dark, isChecked, handleCheckboxChange, onDelete } = props;
+  
+//   function handleClick() {
+//     console.log("passed the id " + id);
+//     onDelete(id);
+//   }
+  
+//   console.log(`Note id: ${id}, isChecked: ${isChecked[id]}`);
+  
+//   return (
+//     <div className={`div-main ${dark ? "div-main-dark" : ""}`}>
+//       &nbsp;
+
+//       <div 
+//         type="text" 
+//         className={`checkbox--label ${isChecked[id] ? "checked--Input" : ""}`}
+//         name="checkbox--tasks" 
+//         checked={isChecked[id]} 
+//         onClick={() => handleCheckboxChange(id)}
+//       /> 
+      
+//       <div className={`div--tasks ${isChecked[id] ? "completed--task" : ""}`}>
+//           {title}
+//       </div>
+
+//       <button className="button__delete" onClick={handleClick}>
+//         <img className="cross__icon" src="../../assets/images/icon-cross.svg" alt="cross icon" />
+//       </button>
+//     </div>
+//   );
+// }
 
 export default Note;
